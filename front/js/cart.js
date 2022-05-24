3ee23b53a1345728ca4e9170b193fc6a3b35557c
Lilou <lidou34@live.fr>
2022-05-24 21:34:07 +0200

    découpage panier en petite fonction, affichage produit, qty, prix total et remove ok

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,4 @@
-let basket =  JSON.parse(localStorage.getItem("articleSelect"));//récupère le produit selectionné 
+let basket =  JSON.parse(localStorage.getItem("articleSelect"));//récupère les articles du LS
 let cartItems = document.getElementById("cart__items");
 let para = document.getElementById("p");
 let cartItem = document.getElementsByClassName("cart__item");
@@ -6,38 +6,36 @@ let containDivImg = document.getElementsByClassName("cart__item__img");
 let divDelete = document.getElementsByClassName("cart__item__content__settings__delete");
 let pDelete = document.getElementsByClassName("deleteItem");
 let inputQte = document.getElementsByClassName("itemQuantity");
-let totalQuantity = document.getElementById("totalQuantity");
-let totalPrice = document.getElementById("totalPrice");
-let prix = 0;
-let quantity = 0;
 
 displayProductBasket(basket);
 
 async function displayProductBasket(basket){
+    //panier vide alert
     if (basket === null || basket == 0) {
         let para = document.createElement("p");
         para.className = "alerte",
         cartItems.append(para);
         para.textContent = "votre panier est vide !!!";
         window.alert("panier vide");
-    }else{
-        
+    }else{//boucle avec fetch pour récupérer image et détail selon d'id de l'article dans le LS
         for(let articleSelect in basket){
             await fetch('http://localhost:3000/api/products/' + basket[articleSelect].id)
                 .then((res) => res.json())
                 .then((data) => (articleData = data))  
                 .catch((error) => console.log(error));
 
+            //création article
             cartItem = document.createElement("article");
             cartItem.className = "cart__item";
             cartItem.setAttribute('data-id', articleData.id);
             cartItem.setAttribute('data-color', articleData.color);
             cartItems.appendChild(cartItem);
 
+                //image
                 containDivImg = document.createElement("div");
                 containDivImg.className = ("cart__item__img");
                 cartItem.appendChild(containDivImg);
-
+                    
                     let img = document.createElement("img");
                         containDivImg.appendChild(img);
                         img.src = articleData.imageUrl;
@@ -46,23 +44,23 @@ async function displayProductBasket(basket){
                 let divContent = document.createElement("div");
                     cartItem.appendChild(divContent);
                     divContent.className = "cart__item__content";
-
+                    
                     let divDescription = document.createElement("div");
                         divContent.appendChild(divDescription);
                         divDescription.className = "cart__item__content__description";
-
+                        //title
                         let title = document.createElement("h2");
                             divDescription.appendChild(title);
                             title.textContent = articleData.name;
-
+                        //color
                         let color =  document.createElement("p");
                             divDescription.appendChild(color);
                             color.textContent = basket[articleSelect].color;
-  
+                    
                     let settings = document.createElement("div");
                         divContent.appendChild(settings);
                         settings.className = "cart__item__content__settings";
-
+                        //quantity
                         let divQuantity =  document.createElement("div");
                             settings.appendChild(divQuantity);
                             divQuantity.className = "cart__item__content__settings__quantity";
@@ -70,7 +68,7 @@ async function displayProductBasket(basket){
                             let pQuantity =  document.createElement("p");
                                 divQuantity.appendChild(pQuantity);
                                 pQuantity.textContent = `Qté : `;
-
+                            
                             let inputQte  =  document.createElement("input");
                                 divQuantity.appendChild(inputQte);
                                 inputQte.className =  "itemQuantity";
@@ -81,101 +79,71 @@ async function displayProductBasket(basket){
                                 inputQte.setAttribute("max", "100");
                                 inputQte.setAttribute("step", "1");
                                 inputQte.setAttribute("value", `${basket[articleSelect].quantity}`);
-
-                                inputQte.addEventListener("input", (event) => {
-                                    quant = event.target.value;
-                                    console.log(quant);
-                                    if(inputQte.value > 0 && inputQte.value < 101 && inputQte.value%1 == 0){
-                                        inputQte.value = quant;
-                                        basket[articleSelect].quantity = Number(quant);
-                                        localStorage.setItem("articleSelect", JSON.stringify(basket));
-                                        basket =  JSON.parse(localStorage.getItem("articleSelect"))
-                                        prix = basket[articleSelect].quantity * articleData.price;
-                                        price.textContent = prix + "€";
-                                    }else{
-                                        inputQte.value = basket[articleSelect].quantity;
-                                    }
-                                });
-                                
+                                //gestion du prix avec la qty de chaque article
                                 let price =  document.createElement("p");
                                     divDescription.appendChild(price);
-                                    let prix = basket[articleSelect].quantity * articleData.price;
+                                let prix = basket[articleSelect].quantity * articleData.price;
                                     price.textContent = prix + "€";
 
-                        divDelete =  document.createElement("div");
-                        settings.appendChild(divDelete);
-                        divDelete.className = "cart__item__content__settings__delete";
-
-                            let pDelete =  document.createElement("p");
+                                //article delete
+                                divDelete =  document.createElement("div");
+                                settings.appendChild(divDelete);
+                                divDelete.className = "cart__item__content__settings__delete";
+                                pDelete =  document.createElement("p");
                                 divDelete.appendChild(pDelete);
                                 pDelete.className = "deleteItem";
                                 pDelete.textContent = "Supprimer";
 
-                                pDelete.addEventListener("click", (article) => {
-                                    basket = localStorage.removeItem(article);
-                                });
+            changeInput();
+                                           
+            deleteItem();
+            //écoute click changement qty avec conditions si ok calcul prix
+            function changeInput(){
+                inputQte.addEventListener("change", (e) => {
+                    quant = e.target.value;
+                    
+                        if(inputQte.value > 0 && inputQte.value < 101 && inputQte.value%1 == 0){
+                            inputQte.value = quant;
+                            basket[articleSelect].quantity = Number(quant);
+                            localStorage.setItem("articleSelect", JSON.stringify(basket));
+                            basket =  JSON.parse(localStorage.getItem("articleSelect"))
+                            prix = basket[articleSelect].quantity * articleData.price;
+                            price.textContent = prix + "€";
+                            location.reload();
+                        }else{
+                            inputQte.value = basket[articleSelect].quantity;
+                        }
+                    }
+                )
+            };
+
+            function deleteItem(articleSelect){
+            //boucle suppression de l'article à l'écoute du click
+                pDelete.addEventListener("click", function() {
+                    //dans le dom
+                    pDelete.closest('.cart__item');
+                    pDelete.remove('.cart__item');
+                    //dans le LS splice modif tableau
+                    basket.splice(articleSelect, 1);
+                    localStorage.setItem('articleSelect', JSON.stringify(basket));
+                    //location.reload();
+                    }
+                )
+            };
+                        
+            function totalQty(){
+                let totalQuantity = document.getElementById("totalQuantity");
+                let totalPrice = document.getElementById("totalPrice"); 
+                let number = 0; //on fixe à zéro   
+                let total = 0;
+                    for (let articleSelect of basket) {
+                        total += articleSelect.quantity * articleData.price;
+                        number += JSON.parse(articleSelect.quantity);
+                    }
+                    totalQuantity.textContent = number;
+                    totalPrice.innerText = total;
+            };
+            totalQty();
         }
     }
-}
-
-
-
-/*function changeQuantity(quantity){//incémenter et décrémenter qte
-    basket;//on récupère le panier existant dans le LS 
-    let foundProduct = basket.find(p => p.id == articleData.id);//vérifier que le produit n'est pas déjà dans le panier et si oui ajouter seulement la quantité
-    if (foundProduct != undefined) { //si il existe déjà dans le panier
-        foundProduct.quantity += quantity;//on ajoute
-        if (foundProduct.quantity <= 0) {//est pluss petit ou égal 0
-            removeFromBasket(foundProduct);//on enlève du panier
-        } else {
-            saveBasket(basket); //pour sauvegarder
-            location.reload(); //rafraichir la page
-            basket =  JSON.parse(localStorage.getItem("articleSelect"))//récupérer les article dans le panier
-             
-        }
-    }
-}
-*/
-/*function saveBasket(basket) {//on crée un panier
-    localStorage.setItem("basket", JSON.stringify(basket)); // avec JSON stringify l'objet est transfomé en JSON chainede caractère (language du LS) enregistre un panier sur le localStorage
-}
-*/
-
-function removeFromBasket(product) {//prendre le panier complet et supprimer un produit grace à son id
-    
-    let basket = basket;//on récupère le panier existant dans le LS
-    basket = basket.filter(p => p.id != product.id);//retirer un élément dont l'id est différent de celui que l'on va passer en paramètre
-    saveBasket(basket); //pour sauvegarder
-    
-}
-
-
-function getNumberProduct() { // function qté totale de produit
-    let number = 0; //on fixe à zéro
-    for(let articleSelect of basket){ // pour chaque produit du panier
-        number += JSON.parse(articleSelect.quantity); // récupère qté pour chaque produit dans le panier les aditionne
-    }
-    return number; //résultat total 
-}
-console.log("mon panier est ", basket);
-console.log("le nombre de produits est ", getNumberProduct());
-
-function getTotalPrice() {
-    let basket = getBasket();//on récupère le panier existant dans le LS 
-    let total = 0;//on fixe à zéro
-    for (let product of basket) { // pour chaque produit du panier
-        total += product.quantity * product.price; // récupère qté multiplie avec le prix pour chaque produit dans le panier les aditionne
-    }
-    return total; //résultat total
-}
-function addLS(basket){
-    //getBasket();
-    
-    changeQuantity(quantity);
-    saveBasket(basket);
-    
-    getTotalPrice();
-}
-
-
-//addLS(basket);
+}; 
